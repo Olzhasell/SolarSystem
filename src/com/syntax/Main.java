f38a86e4220add9fea53add4a160f04aa46012d3
Olzhasell <olzhas.ell@gmail.com>
2021-11-06 10:56:37 -0400

    Solar System Basic Structure is Ready

---
+++ src/com/syntax/Main.java
@@ -0,0 +1,14 @@
+package com.syntax;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        SolarSystem andromeda=new SolarSystem();
+        andromeda.sun=1;
+        andromeda.planets=2;
+        andromeda.moon=4;
+
+    }
+}
